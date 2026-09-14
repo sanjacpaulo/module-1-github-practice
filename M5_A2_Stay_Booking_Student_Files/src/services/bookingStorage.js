2b8496cc3ab5fdb8a5fe420b03d5d44b7064aebe
sanjacpaulo <quirozbayona.p184123@stu.sanjac.edu>
2026-09-14 21:36:45 +0000

    Add booking presistence service

--- M5_A2_Stay_Booking_Student_Files/src/services/bookingStorage.js
+++ M5_A2_Stay_Booking_Student_Files/src/services/bookingStorage.js
@@ -6,6 +6,7 @@ export const SELECTED_CITY_KEY = 'stayfinder-selected-city';
 export async function saveHotels(hotels) {
   // TODO 1:
   // Save hotels using SAVED_HOTELS_KEY and JSON.stringify().
+  await AsyncStorage.setItem(SAVED_HOTELS_KEY, JSON.stringify(hotels));
 }
 
 export async function loadHotels() {
@@ -13,20 +14,32 @@ export async function loadHotels() {
   // Load SAVED_HOTELS_KEY.
   // Return [] if nothing has been saved.
   // Otherwise return JSON.parse(storedValue).
+  const storedValue = await AsyncStorage.getItem(SAVED_HOTELS_KEY);
+  if (storedValue === null) {
+    return [];
+  }
+  return JSON.parse(storedValue);
 }
 
 export async function clearSavedHotels() {
   // TODO 3:
   // Remove only SAVED_HOTELS_KEY.
+  await AsyncStorage.removeItem(SAVED_HOTELS_KEY);
 }
 
 export async function saveSelectedCity(cityId) {
   // TODO 4:
   // Save cityId using SELECTED_CITY_KEY.
+  await AsyncStorage.setItem(SELECTED_CITY_KEY, cityId);
 }
 
 export async function loadSelectedCity() {
   // TODO 5:
   // Load SELECTED_CITY_KEY.
   // Return 'houston' if nothing has been saved.
+  const storedValue = await AsyncStorage.getItem(SELECTED_CITY_KEY);
+  if (storedValue === null) {
+    return 'houston';
+  }
+  return storedValue;
 }
